9217c7c0d3cc2c64709f471494d394827fcdabd4
anderrf <ander_rochafarias@live.com>
2020-07-18 18:39:36 -0300

    Aplicacao da validacao dos valores

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  private form:FormGroup;
+  private valA:number;
+  private valB:number;
+  private valC:number;
+  private delta:number;
+  private res1:number;
+  private res2:number;
+  private answer:string = "adads";
+
+  constructor(private fb:FormBuilder) 
+  {
+    this.form = this.fb.group({
+      valA: ['', Validators.compose([
+        Validators.required
+      ])],
+      valB: ['', Validators.compose([
+        Validators.required
+      ])],
+      valC: ['', Validators.compose([
+        Validators.required
+      ])]
+    });
+  }
+
+  get getAnswer():string
+  {
+    return this.answer;
+  }
 
 }
